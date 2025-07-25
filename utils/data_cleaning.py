ae8041ad6fe1760434a7980efccd02071d38e010
Shamel Bhimani <shamelbhimani@outlook.com>
2025-07-25 16:12:19 -0400

    ADDED: feature remapping function

--- utils/data_cleaning.py
+++ utils/data_cleaning.py
@@ -12,3 +12,22 @@ def remap_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
     """
     df.columns = columns
     return df
+
+def remap_features(df: pd.DataFrame,
+                   index_map: dict[str, dict[str, str]]) -> pd.DataFrame | str:
+    """
+    This function renames the features of each column based on the
+    information associated with each column in the nested dictionary. The
+    nested dictionary is provided by the user and is *not* hardcoded for
+    customizability.
+    :param df:
+    :param index_map:
+    :return:
+    """
+    for column, mapping in index_map.items():
+        if column in df.columns:
+            df[column] = df[column].map(mapping)
+        else:
+            print(f'Column {column} not found in DataFrame. Skipping to next '
+                'column.')
+    return df
